0b6d5523b17acaa194c69ae34615f834daaa567f
mvbaryn <mvbaryn@gmail.com>
2026-03-25 01:41:07 +0300

    update

---
+++ shop/main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
